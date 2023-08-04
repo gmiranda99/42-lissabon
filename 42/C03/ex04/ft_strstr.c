89b391f0e6022374d7efad36797a91c0c6d584a5
gmiranda99 <141457515+gmiranda99@users.noreply.github.com>
2023-08-04 22:30:34 +0100

    Update ft_strstr.c

--- 42/C03/ex04/ft_strstr.c
+++ 42/C03/ex04/ft_strstr.c
@@ -22,12 +22,12 @@ char	*ft_strstr(char *str, char *to_find)
 		return (str);
 	while (str[i] != '\0')
 	{
-		j = 0;;
+		j = 0;
 		if (str[i + j] == to_find[j])
 		{
 			while (str[i + j] == to_find[j])
 			{
-				j++;
+				j = ++;
 				if (to_find[j] == '\0')
 				{
 					return (str + i);
